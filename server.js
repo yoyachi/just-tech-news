523eb7d6dcfc92f9f8458c0919c4fe972b90f3fd
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-03-23 13:08:04 -0500

    add a homepage

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
+const path = require("path");
 const express = require('express');
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+
+
+const routes = require('./controllers');
+
+const { dirname } = require("node:path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(--dirname, "public")));
 
 // turn on routes
 app.use(routes);
